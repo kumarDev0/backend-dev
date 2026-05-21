98efb35df99f34cda4d68fa6c3d516f7d30621e0
kumarDev0 <villainxcoding9010@gmail.com>
2026-05-21 21:42:34 +0530

    Different Different modules type

---
+++ Different-Type-module/app.js
@@ -0,0 +1,22 @@
+
+// Native module or core modules
+import fs from 'node:fs'
+import net from 'node:net'
+import dgram from 'node:dgram'
+import http from 'node:http'
+import https from 'node:https'
+import child_process from 'node:child_process'
+import cluster from 'node:cluster'
+import zlib from 'node:zlib'
+import worker_threads from 'node:worker_threads'
+import crypto from 'node:crypto'
+
+
+// user modules
+import { num } from './math.js'
+
+// NPM modules
+import express from 'express'
+
+
+
---
+++ Different-Type-module/math.js
@@ -0,0 +1,4 @@
+
+export const num = 1234
+
+
