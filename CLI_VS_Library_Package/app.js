7207a2da552cb95b0fec7d966c846f788bdd1d93
kumarDev0 <villainxcoding9010@gmail.com>
2026-05-23 13:13:09 +0530

    CLI andLibrary Package and Dependency and Dev dependency

---
+++ CLI_VS_Library_Package/app.js
@@ -0,0 +1,38 @@
+// Library package
+
+import axios from "axios";
+console.log(axios);
+
+// const axios = require("axios");
+// console.log(axios);
+
+// CLI package hum tab install karte hai jab hume development
+// me help chahiye isko hum devDependencies me add karte hai 
+// (npm i vite -D) cli ya libaray package jo hote hai wo locally install hota hai
+// node_module folder me jab bhi laibrary ya cli package install hota hai toh wo locally install hota hai
+// locally ka matlab current folder se hi aap ise access kar skte ho 
+// current folder ke bahar access nhi kar skte hai by default ye dono package locally install hote hai
+// lekin agar aap globally install karenge toh isko pure system me kahi se bhi access and run kar skte hai 
+// koi bhi directory se ise access ya run kar skte hai 
+// (npm i vite -D -g) globally install karne ke liye g flag use karte hai 
+// (npm i axios -g) 
+
+// yaha pe jo humne two command diya hai (-D) ka matlab ye devdependency me install hoga aur ye CLI package hai isko sirf terminal se use kar skte hai
+// (npm i axios -g) ye hum depency ke andar install kar rhe hai ye by defaly dependency ke andar install hota hai 
+// ye hamara library package hai hum isko code me use karte hai
+//common js and module ki tarah (import and require)
+
+// vite ek server hai ye hame apne project ko live server pe dekhne ka faci;ity deta hai
+// ye local host pe run karta hai 
+
+//unistall karne ke liye (npm  un vite -D)
+// local and global package
+
+
+
+// humne isme teen chije install kiya tha
+//1 axios , date-fns , vite 
+//humne ye teeno unistall kar diya 
+
+// npm un axios , npm un date-fns , npm un vite -g
+// npm un vite -D 
