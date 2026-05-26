09010afca12163860bbd64836d868bcc0b566643
kumarDev0 <villainxcoding9010@gmail.com>
2026-05-26 18:30:12 +0530

    i learn fs module copyFile , rename , cp, unlink, rmdir , rm , mkdir, writeFile, stat, readFile , watch

--- More_File_operation/app.js
+++ More_File_operation/app.js
@@ -15,49 +15,49 @@ import {
 
 // rename('code.png' , 'code2.png')
 
-// copyFile('code2.png' , 'mausham.png')
-// copyFile('mausham.png' , "C:\\Users\\VILLAN\\OneDrive\\Desktop\\janeman.png")
+copyFile('code2.png', 'mausham.png')
+copyFile('mausham.png', "C:\\Users\\VILLAN\\OneDrive\\Desktop\\janeman.png")
 
 
 // copy folder
-// cp('./src' , "C:\\Users\\VILLAN\\OneDrive\\Desktop\\src" ,{recursive : true})
+cp('./src', "C:\\Users\\VILLAN\\OneDrive\\Desktop\\src", { recursive: true })
 
 
 // move and rename karne ke liye rename() method ka hi use hoga
 
-// rename('mausham.png' , "C:\\Users\\VILLAN\\OneDrive\\Desktop\\mausham2.png" )
-// console.log("move and rename ek sath chalega")
+rename('mausham.png', "C:\\Users\\VILLAN\\OneDrive\\Desktop\\mausham2.png")
+console.log("move and rename ek sath chalega")
 
 // delete file
 
-// unlink('mausham2.png')
-// console.log("deleted permanently not recover")
+unlink('mausham2.png')
+console.log("deleted permanently not recover")
 
 // delete directory (rmdir , rm)
 // rmdir empty directory ko delete karta hai
 // rm non empty directory ko delete karta hai
 
-// rmdir('debug')
-// console.log("deleted permanently not recover")
+rmdir('debug')
+console.log("deleted permanently not recover")
 
-// rm('src' , {recursive : true})
-// console.log("deleted permanently not recover")
+rm('src', { recursive: true })
+console.log("deleted permanently not recover")
 
 
 // mkdir  create empty directory and writeFile will create file
 
-// mkdir('control')
-// console.log(" Empty irectory created")
+mkdir('control')
+console.log(" Empty irectory created")
 
-// writeFile('style.css' , '')
-// console.log("Emty file created")
+writeFile('style.css', '')
+console.log("Emty file created")
 
-// mkdir('sum' ,  writeFile('index.html' , '') )
-// console.log("Directory and file created ")
+mkdir('sum', writeFile('index.html', ''))
+console.log("Directory and file created ")
 
 // ye hame size and birthdate and time batata hai file and directory ka
-// const stats = await stat('style.css')
-// console.log(stats)
+const stats = await stat('style.css')
+console.log(stats)
 
 
 // watch method ye ek file ko watch karta hai file me koi change hua hai ya nhi 
@@ -77,11 +77,11 @@ for await (const event of watcher) {
 
 // difference 2
 
-// import { watch } from "node:fs"
-// import { readFile } from "node:fs/promises"
+import { watch } from "node:fs"
+import { readFile } from "node:fs/promises"
 
-// watch('file.txt', async (eventType) => {
-//     if(eventType === "change"){
-//         console.log(await readFile('file.txt', "utf-8"))
-//     }
-// })
+watch('file.txt', async (eventType) => {
+    if (eventType === "change") {
+        console.log(await readFile('file.txt', "utf-8"))
+    }
+})
